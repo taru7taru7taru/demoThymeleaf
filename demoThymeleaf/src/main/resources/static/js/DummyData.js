ff47645ca0535de85ffacbda09655ff60002f0cb
taru7taru7taru <taru2taru@gmail.com>
2018-07-06 11:39:37 +0900

    Handsontableの使い方を調査

--- demoThymeleaf/src/main/resources/static/js/DummyData.js
+++ demoThymeleaf/src/main/resources/static/js/DummyData.js
@@ -6,6 +6,34 @@ var spread_data= [
 		['くり',3],
 		['ｽｲｯﾁ',2]
 ];
+var spread_2_supplement= [
+    ['国籍', 'アメリカ', '日本', '日本', '日本', '日本', 'アメリカ','日本','イタリア'],
+    ['補助情報1', 'aaaa1', 'bbbb1', 'cccc1', 'dddd1', 'eeee1', 'ffff1','gggg1','hhhh1'],
+    ['補助情報2', 'aaaa2', 'bbbb2', 'cccc2', 'dddd2', 'eeee2', 'ffff2','gggg2','hhhh2'],
+    ['補助情報3', 'aaaa3', 'bbbb3', 'cccc3', 'dddd3', 'eeee3', 'ffff3','gggg3','hhhh3'],
+    ['補助情報4', 'aaaa4', 'bbbb4', 'cccc4', 'dddd4', 'eeee4', 'ffff4','gggg4','hhhh4'],
+    ['補助情報5', 'aaaa5', 'bbbb5', 'cccc5', 'dddd5', 'eeee5', 'ffff5','gggg5','hhhh5'],
+];
+
+var spread_2_header= [
+    ['', 'Tesla', 'Nissan', 'Toyota', 'Honda', 'Mazda', 'Ford','Suzuki','Fiat'],
+];
+
+var spread_2_data= [
+    ['2017-04-01', 10, 11, 12, 13, 15, 16, 17, 18],
+    ['2018-04-01', 10, 11, 12, 13, 15, 16, 17, 18],
+    ['2019-04-01', 10, 11, 12, 13, 15, 16, 17, 18],
+    ['2020-04-01', 10, 11, 12, 13, 15, 16, 17, 18],
+    ['2021-04-01', 10, 11, 12, 13, 15, 16, 17, 18]
+];
+
+spread_3_data = [
+    {id: 1, name: 'Ted Right', address: ''},
+    {id: 2, name: 'Frank Honest', address: ''},
+    {id: 3, name: 'Joan Well', address: ''},
+    {id: 4, name: 'Gail Polite', address: ''},
+    {id: 5, name: 'Michael Fair', address: ''},
+]
 
  var d3_data = [
 	 {'mTime': '2018-06-26T10:00','val': 100 },
